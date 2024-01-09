2b6205a80affb8f476be55ad9d9021228892a5d9
karnking <karankotai9183@gmail.com>
2024-01-09 17:01:59 +0530

    setup layout

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,8 +1,12 @@
-export default function layout() {
+export default function Layout({children}:{children:React.ReactNode}) {
     return (
         <div>
-            <div className="w-1/4"></div>
-            <div className="w-3/4"></div>
+            <div className="w-1/4">
+                left panel
+            </div>
+            <div className="w-3/4">
+                {children}
+            </div>
         </div>
     )
 }
